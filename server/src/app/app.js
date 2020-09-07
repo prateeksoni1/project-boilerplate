a27bd6181a03a7c852036d8fa8c74391616e3609
proforty7 <proforty7@gmail.com>
2020-09-08 02:46:52 +0530

    enabled middlewares. add readme

--- server/src/app/app.js
+++ server/src/app/app.js
@@ -1,7 +1,15 @@
 const express = require('express');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const cors = require('cors');
 const { notFound, errorHandler } = require('../middlewares');
 
 const app = express();
+
+app.use(cors());
+
+app.use(helmet());
+app.use(morgan('dev'));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
